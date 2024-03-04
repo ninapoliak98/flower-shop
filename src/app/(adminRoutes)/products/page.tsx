1b002bce2f6fbba65f553df2d21b4d7cc8c28581
Nina Poliak-Karatkevich <ninapoliakkarat@gmail.com>
2024-03-04 16:04:03 -0500

    started product form

--- src/app/(adminRoutes)/products/page.tsx
+++ src/app/(adminRoutes)/products/page.tsx
@@ -1,9 +1,38 @@
 import { FC } from 'react';
+import { prisma } from '@/prisma';
+import Image from 'next/image';
 
-const ProductsPage: FC = () => {
+const ProductsPage: FC = async () => {
+  const user = await prisma.user.findFirst({
+    where: {
+      email: 'test@test.com'
+    }
+  })
+
   return (
     <div>
-      Products
+      <form action=''>
+        <div>
+          <Image src={''} alt={''} />
+        </div>
+        <div>
+          <div>
+            <input type="file" name="productImage" />
+            <input type="file" name="productImage" />
+            <input type="file" name="productImage" />
+          </div>
+          <div>
+            <input type="text" name='name' placeholder='Name...' />
+            <input type="text" name='short-desc' placeholder='short description' />
+            <input type="text" name='description' placeholder='Description..' />
+            <input type="text" name='price' placeholder='price' />
+          </div>
+        </div>
+        <div>
+          <button>Available</button>
+          <button>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
